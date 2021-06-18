98d96b8fd96ebce3f62177008edfde0596f42407
lujiangtao.ljt <lujiangtao.ljt@antgroup.com>
2021-06-18 22:08:06 +0800

    hello controller！

--- client-common/src/main/java/org/example/controller/HelloController.java
+++ client-common/src/main/java/org/example/controller/HelloController.java
@@ -9,6 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @RequestMapping("/hello")
     public String Hello() {
-        return "hello";
+        return "hello！";
     }
 }
